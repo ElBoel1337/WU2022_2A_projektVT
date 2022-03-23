bd077da4ae8a77232681837ef0be40fb9d5f959b
JoelDinBoel <joel.rambergthemelis@elev.ga.ntig.se>
2022-03-23 13:27:03 +0100

    fef
    cdc

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("Slides");
+  var x = document.getElementsByClassName("slides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
@@ -29,17 +29,3 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
-/*html kod*/
-/*            <div class="Slides">
-                <img src="img/LAN.bild1.jpg" class="mySlides" style="width:100%">
-            </div>
-            <div class="Slides">
-                <img src="img/LAN.bild2.jpg" class="mySlides" style="width:100%">
-            </div>
-            <div class="Slides">
-                <img src="img/LAN.bild3.jpeg" class="mySlides" style="width:100%">
-            </div>
-
-            <button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
-            <button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
-*/
